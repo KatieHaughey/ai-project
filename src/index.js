c199494fa13ee374d409d1d59c84ae4365f546f1
KatieHaughey <132608324+KatieHaughey@users.noreply.github.com>
2025-06-09 16:23:19 +0100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -12,10 +12,15 @@ function answerQuestion(event) {
 
     let question = document.querySelector("#question");
     
+    
     let key = "342b2de303cf071e1dea170f944t769o";
     let prompt = `Find official documentation on WCAG 2.2 AA accessibility guidelines and generate an answer to ${question.value}`;
-    let context = "Only using references from w3.org, explain your answer in a clear, simple and friendly tone. Structure your answer with semantic HTML that is compliant with the WCAG 2.2 AA guidelines. Use an example. Link to the specific relevant WCAG guideline in a new tab. Add links openining in a new tab to referenced used to the end of the answer. Limit to 175 words.";
+    let context = "Only using references from w3.org, explain your answer in a clear, simple and friendly tone. Structure your answer with semantic HTML that is compliant with the WCAG 2.2 AA guidelines. Use an example. Link to the specific WCAG guideline relevant to the question in a new tab. Add links openining in a new tab to referenced used to the end of the answer. Limit to 175 words.";
     let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+    let answer = document.querySelector("#answer");
+    answer.classList.remove("hidden");
+    answer.innerHTML = "Searching documentation... 🔍"
 
     axios.get(url).then(displayAnswer);
 }
