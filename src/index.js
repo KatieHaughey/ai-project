84bf7736cc8d97ac056b71285f7109929b9c6f21
KatieHaughey <132608324+KatieHaughey@users.noreply.github.com>
2025-06-10 09:20:55 +0100

    example question added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayAnswer(response) {
-     new Typewriter("#answer", {
+    new Typewriter("#answer", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -15,7 +15,7 @@ function answerQuestion(event) {
     
     let key = "342b2de303cf071e1dea170f944t769o";
     let prompt = `Find official documentation on WCAG 2.2 AA accessibility guidelines and generate an answer to ${question.value}`;
-    let context = "Only using references from w3.org, explain your answer in a clear, simple and friendly tone. Structure your answer with semantic HTML that is compliant with the WCAG 2.2 AA guidelines. Use a 'how to' example in response to the question asked. Avoid code tags and just use plain English (UK). Link to the specific WCAG guideline relevant to the question in a new tab. Add links openining in a new tab to referenced used to the end of the answer. Limit to 175 words.";
+    let context = "Only using references from w3.org, explain your answer in a clear, simple and friendly tone. Structure your answer with semantic HTML that is compliant with the WCAG 2.2 AA guidelines. If using headings, do not add inside a list. Use a 'how to' example in response to the question asked. Avoid code tags and just use plain English (UK) so people with no coding knowledge understand. Link to the specific WCAG guideline relevant to the question in a new tab. Add links openining in a new tab to referenced used to the end of the answer. Limit to 175 words.";
     let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
     let answer = document.querySelector("#answer");
