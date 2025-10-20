4b4aab1a9fc5d5818f636678722a7cf3df1f0334
KatieHaughey <132608324+KatieHaughey@users.noreply.github.com>
2025-10-20 09:31:36 +0100

    SVGs added to folder

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 function displayAnswer(response) {
-    new Typewriter("#answer", {
+  new Typewriter("#answer", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     curser: "",
-    });
+  });
 }
 
 function answerQuestion(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let question = document.querySelector("#question");
-    
-    
-    let key = "342b2de303cf071e1dea170f944t769o";
-    let prompt = `Find official documentation on WCAG 2.2 AA accessibility guidelines and generate an answer to ${question.value}`;
-    let context = "Only using references from w3.org, explain your answer in a clear, simple and friendly tone. Structure your answer with semantic HTML that is compliant with the WCAG 2.2 AA guidelines. If using headings, do not add inside a list. Use a 'how to' example in response to the question asked. Avoid code tags and just use plain English (UK) so people with no coding knowledge understand. Link to the specific WCAG guideline relevant to the question in a new tab. Add links openining in a new tab to referenced used to the end of the answer. Limit to 175 words.";
-    let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+  let question = document.querySelector("#question");
 
-    let answer = document.querySelector("#answer");
-    answer.classList.remove("hidden");
-    answer.innerHTML = "Searching documentation... 🔍"
+  let key = "342b2de303cf071e1dea170f944t769o";
+  let prompt = `Find official documentation on WCAG 2.2 AA accessibility guidelines and generate an answer to ${question.value}`;
+  let context =
+    "Only using references from w3.org, explain your answer in a clear, simple and friendly tone. Structure your answer with accessibly semantic HTML that is compliant with the WCAG 2.2 AA guidelines. If using headings, do not add inside a list. Use a 'how to' example in response to the question asked. Use plain English (UK) so people with no coding knowledge understand answers. Link to the specific WCAG guideline relevant to the question in a new tab and use accessible link text. Add links openining in a new tab to references used to the end of the answer. Only link to publicly accessible urls. Do not reference https://www.w3.org/WAI/WCAG22/. Limit to 175 words.";
+  let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
-    axios.get(url).then(displayAnswer);
+  let answer = document.querySelector("#answer");
+  answer.classList.remove("hidden");
+  answer.innerHTML = "Searching documentation... 🔍";
+
+  axios.get(url).then(displayAnswer);
 }
 
 let questionForm = document.querySelector("#question-form");
 questionForm.addEventListener("submit", answerQuestion);
-
-
